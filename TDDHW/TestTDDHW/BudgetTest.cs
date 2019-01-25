1cc3855965ea3761bcf102d4eb623de0c93f5d62
Jay <jay@reallusion.com>
2019-01-25 16:27:14 +0800

    先開出空專案

---
+++ TDDHW/TestTDDHW/BudgetTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDHW;
+
+namespace TDDHWTest
+{
+    [TestClass]
+    public class BudgetTest
+    {
+        [TestMethod]
+        public void AddBudgetTest()
+        {
+            //Arrange
+            //Act
+            Budget b = new Budget();
+            //Assert
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ TDDHW/TestTDDHW/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestTDDHW")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("TestTDDHW")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c09fc22b-fce2-4c78-92c7-68c8349d4fba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
