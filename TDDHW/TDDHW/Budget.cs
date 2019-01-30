7c870eb266f57d131b6a2174be3ea2c0fb61f39a
LewisHsu <lewishsu@reallusion.com>
2019-01-30 10:27:24 +0800

    - 主要功能 (1.設定各月份預算 2.輸入查詢月份 3.回傳預算總額) - 測試function

--- TDDHW/TDDHW/Budget.cs
+++ TDDHW/TDDHW/Budget.cs
@@ -8,5 +8,59 @@ namespace TDDHW
 {
     public class Budget
     {
+        // key: 預算月份, value: 每日預算
+        private Dictionary<DateTime, int> BudgetList = new Dictionary<DateTime, int>();
+
+        private DateTime QueryStart { get; set; }
+        private DateTime QueryEnd { get; set; }
+
+        public bool SetMonthlyAmount(Dictionary<DateTime, int> dictionary)
+        {
+            bool isRightAmount = false;
+            foreach (KeyValuePair<DateTime, int> pair in dictionary)
+            {
+                // 預算必須整除當月天數
+                isRightAmount = (pair.Value % DateTime.DaysInMonth(pair.Key.Year, pair.Key.Month) == 0);
+                if (isRightAmount)
+                {
+                    BudgetList.Add(pair.Key, pair.Value / DateTime.DaysInMonth(pair.Key.Year, pair.Key.Month));
+                }
+            }
+
+            return isRightAmount;
+        }
+
+        public bool SetQueryRange(DateTime start, DateTime end)
+        {
+            bool isRightRange = false;
+            // 起始時間需小於結束時間
+            if (start <= end)
+            {
+                isRightRange = true;
+            }
+
+            if (isRightRange)
+            {
+                QueryStart = start;
+                QueryEnd = end;
+            }
+            return isRightRange;
+        }
+
+        public int GetBudget()
+        {
+            int totalBudget = 0;
+            foreach (KeyValuePair<DateTime, int> pair in BudgetList)
+            {
+                // check overlap 
+                if (pair.Key <= QueryEnd && QueryStart <= pair.Key.AddMonths(1).AddDays(-1))
+                {
+                    DateTime overlapStart = new DateTime(Math.Max(pair.Key.Ticks, QueryStart.Ticks));
+                    DateTime overlapEnd = new DateTime(Math.Min(pair.Key.AddMonths(1).AddDays(-1).Ticks, QueryEnd.Ticks));
+                    totalBudget += (pair.Value * ((overlapEnd - overlapStart).Days + 1));
+                }
+            }
+            return totalBudget;
+        }
     }
 }
--- TDDHW/TestTDDHW/BudgetTest.cs
+++ TDDHW/TestTDDHW/BudgetTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TDDHW;
 
@@ -8,13 +9,108 @@ namespace TDDHWTest
     public class BudgetTest
     {
         [TestMethod]
-        public void AddBudgetTest()
+        public void AddRightBudgetTest()
         {
-            //Arrange
             //Act
-            Budget b = new Budget();
+            Budget budget = new Budget();
+
             //Assert
-            Assert.AreEqual(1, 1);
+            Assert.AreEqual(budget.SetMonthlyAmount(new Dictionary<DateTime, int>()
+            {
+                { new DateTime(2019, 1, 1), 31 }
+            }), true);
+        }
+        [TestMethod]
+        public void AddWrongBudgetTest()
+        {
+            //Act
+            Budget budget = new Budget();
+
+            //Assert
+            Assert.AreEqual(budget.SetMonthlyAmount(new Dictionary<DateTime, int>()
+            {
+                { new DateTime(2019, 1, 1), 100 }
+            }), false);
+        }
+        [TestMethod]
+        public void SetRightRangeTest()
+        {
+            //Act
+            Budget budget = new Budget();
+
+            //Assert
+            Assert.AreEqual(budget.SetQueryRange(new DateTime(2019, 1, 1), new DateTime(2019, 1, 31)), true);
+        }
+        [TestMethod]
+        public void SetWrongRangeTest()
+        {
+            //Act
+            Budget budget = new Budget();
+
+            //Assert
+            Assert.AreEqual(budget.SetQueryRange(new DateTime(2019, 1, 31), new DateTime(2019, 1, 1)), false);
+        }
+        [TestMethod]
+        public void GetSingleMonthBudget()
+        {
+            Budget budget = new Budget();
+            budget.SetMonthlyAmount(new Dictionary<DateTime, int>()
+            {
+                { new DateTime(2019, 1, 1), 31 }
+            });
+            budget.SetQueryRange(new DateTime(2019, 1, 1), new DateTime(2019, 1, 31));
+
+            Assert.AreEqual(budget.GetBudget(), 31);
+        }
+        [TestMethod]
+        public void GetLessThanOneMonthBudget()
+        {
+            Budget budget = new Budget();
+            budget.SetMonthlyAmount(new Dictionary<DateTime, int>()
+            {
+                { new DateTime(2019, 1, 1), 31 }
+            });
+            budget.SetQueryRange(new DateTime(2019, 1, 1), new DateTime(2019, 1, 10));
+
+            Assert.AreEqual(budget.GetBudget(), 10);
+        }
+        [TestMethod]
+        public void GetCrossMonthBudget()
+        {
+            Budget budget = new Budget();
+            budget.SetMonthlyAmount(new Dictionary<DateTime, int>()
+            {
+                { new DateTime(2019, 1, 1), 31 }
+            });
+            budget.SetQueryRange(new DateTime(2019, 1, 1), new DateTime(2019, 2, 28));
+
+            Assert.AreEqual(budget.GetBudget(), 31);
+        }
+        [TestMethod]
+        public void GetMulitpleMonthBudget()
+        {
+            Budget budget = new Budget();
+            budget.SetMonthlyAmount(new Dictionary<DateTime, int>()
+            {
+                { new DateTime(2019, 1, 1), 31 },
+                { new DateTime(2019, 2, 1), 56 },
+                { new DateTime(2019, 3, 1), 93 },
+            });
+            budget.SetQueryRange(new DateTime(2019, 1, 22), new DateTime(2019, 3, 10));
+
+            Assert.AreEqual(budget.GetBudget(), 96);
+        }
+        [TestMethod]
+        public void GetNotSettingMonthBudget()
+        {
+            Budget budget = new Budget();
+            budget.SetMonthlyAmount(new Dictionary<DateTime, int>()
+            {
+                { new DateTime(2019, 2, 1), 28 }
+            });
+            budget.SetQueryRange(new DateTime(2019, 1, 1), new DateTime(2019, 1, 31));
+
+            Assert.AreEqual(budget.GetBudget(), 0);
         }
     }
 }
