4faf652d0aa67fd3162e77a8166180083e7623b8
ReallusionLeoChen <47025226+ReallusionLeoChen@users.noreply.github.com>
2019-02-01 12:18:16 +0800

    Leo上一次TDDHomeWork版本
    *Leo上一次TDDHomeWork版本
    input: 輸入月份, 預算, 查詢天數起訖日期。
    output: 該天數內的預算。
    - 先測試VeriftyDctBudgetData測試預算是否能整除月份天數。
    -

--- TDDHW/TDDHW/Budget.cs
+++ TDDHW/TDDHW/Budget.cs
@@ -8,5 +8,45 @@ namespace TDDHW
 {
     public class Budget
     {
+        private DateTime dateStart;
+        private DateTime dateEnd;
+        Dictionary<int, int> BudgetData = new Dictionary<int, int>();
+
+        public void MonthlyBudget(Dictionary<int, int> monthlyBudget)
+        {
+            BudgetData = monthlyBudget;
+        }
+
+        public void DateStart(DateTime dDateStart)
+        {
+            dateStart = dDateStart;
+        }
+
+        public void DateEnd(DateTime dDateEnd)
+        {
+            dateEnd = dDateEnd;
+        }
+
+        public bool VeriftyDctBudgetData()
+        {
+            bool verified = true;
+            int days = 0;
+            foreach (var item in BudgetData)
+            {
+                int month = item.Key;
+                int budget = item.Value;
+                days = DateTime.DaysInMonth(2019, month);
+                if (budget % days != 0)
+                {
+                    verified = false;
+                }   
+            }
+            return verified;
+        }
+
+        public int GetBudgetResult()
+        {
+            return 0;
+        }
     }
 }
--- TDDHW/TestTDDHW/BudgetTest.cs
+++ TDDHW/TestTDDHW/BudgetTest.cs
@@ -1,20 +1,47 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TDDHW;
+using System.Collections.Generic;
 
 namespace TDDHWTest
 {
     [TestClass]
     public class BudgetTest
     {
+        Budget budget = new Budget();
+
         [TestMethod]
-        public void AddBudgetTest()
+        public void TestDctData() //測試預算是否能整除月份天數
         {
             //Arrange
-            //Act
-            Budget b = new Budget();
+            Dictionary<int, int> dctMonthlyBudget = new Dictionary<int, int>();
+            dctMonthlyBudget.Add(2, 56);
+            dctMonthlyBudget.Add(3, 31);
+            budget.MonthlyBudget(dctMonthlyBudget);
+
+            bool verified = budget.VeriftyDctBudgetData();
+
             //Assert
-            Assert.AreEqual(1, 1);
+            Assert.AreEqual(verified, true);
+        }
+
+        [TestMethod]
+        public void TestSingleMonth()
+        {
+            //Arrange
+            Dictionary<int, int> dctMonthlyBudget = new Dictionary<int, int>();
+            dctMonthlyBudget.Add(2, 28);
+            budget.MonthlyBudget(dctMonthlyBudget);
+
+            DateTime dStart = new DateTime(2019, 02, 01);
+            DateTime dEnd = new DateTime(2019, 02, 28);
+            budget.DateStart(dStart);
+            budget.DateEnd(dEnd);
+
+            int periodBudget = budget.GetBudgetResult();
+
+            //Assert
+            Assert.AreEqual(periodBudget, 0);
         }
     }
 }
